c38e5442f7d697ccc0c91b8cf0b2a6ffb323f598
Rodolfo Guerrero Cisterna <140962839+Ragnarwof94@users.noreply.github.com>
2025-11-27 19:52:12 -0300

    Merge upstream/master into b-rodolfoguerrero

--- 8_Bucles/8_1_For.py
+++ 8_Bucles/8_1_For.py
@@ -1,18 +1,12 @@
-#➡️ Un ciclo for es una estructura de control que permite repetir un bloque de código un número determinado de veces.
-#➡️ Ejemplo de un ciclo for en Python
-#➡️ Imprime los números del 0 al 9 utilizando un ciclo for y la función range()
-#➡️ La función range(0, 10) genera una secuencia de números desde 0 hasta 9
-#➡️ El ciclo for itera sobre cada número en la secuencia y lo imprime
-#➡️ Inicialización de la variable numero
-numero = 1
+#➡️ Muestra la tabla de multiplicar de ese número del 0 al 9
+#➡️ Utiliza un ciclo for para generar la tabla de multiplicar
+#➡️ Cada iteración del ciclo multiplica el número ingresado por el índice del ciclo
+#➡️ Imprime el resultado de cada multiplicación
+#➡️ Solicita al usuario que ingrese un número
+numero = int(input("Ingrese un número: "))
 #➡️ Ciclo for para iterar desde 0 hasta 9
-#➡️ Imprime cada número en la secuencia
-for numero in range(0, 10):
-    #➡️ Imprime el número actual
-    #➡️ La variable numero toma el valor de cada elemento en la secuencia generada por range()
-    #➡️ El ciclo se repite hasta que se hayan impreso todos los números en la secuencia
-    #➡️ Al finalizar el ciclo, la variable numero tendrá el valor del último número en la secuencia
-    #➡️ Imprime el número actual en cada iteración
-    #➡️ Al finalizar el ciclo, la variable numero tendrá el valor 9
-    #➡️ Imprime el valor final de la variable numero después del ciclo
-    print(numero)
+for i in range(0, 10):
+    #➡️ Calcula la multiplicación del número ingresado por el índice del ciclo
+    multiplicacion = numero * i
+    #➡️ Imprime el resultado de la multiplicación
+    print (multiplicacion)
